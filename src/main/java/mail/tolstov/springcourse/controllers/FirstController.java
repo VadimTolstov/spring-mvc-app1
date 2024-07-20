faad354ed5a137ddb5854111fc1c6e8caec469f9
VadimTolstov <tolstov50@mail.ru>
2024-07-20 09:12:04 +0300

    не работает

--- src/main/java/mail/tolstov/springcourse/controllers/FirstController.java
+++ src/main/java/mail/tolstov/springcourse/controllers/FirstController.java
@@ -2,8 +2,10 @@ package mail.tolstov.springcourse.controllers;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Component
+@RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage() {
