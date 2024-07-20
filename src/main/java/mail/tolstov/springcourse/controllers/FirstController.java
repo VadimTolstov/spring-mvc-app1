8a2b3094f32fc0f9ab59de0a3fdac5d1438f4e1d
VadimTolstov <tolstov50@mail.ru>
2024-07-20 14:19:38 +0300

    работает lesson-20

--- src/main/java/mail/tolstov/springcourse/controllers/FirstController.java
+++ src/main/java/mail/tolstov/springcourse/controllers/FirstController.java
@@ -1,25 +1,20 @@
 package mail.tolstov.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-//таким образом можно получить все данные от запроса
-//    public String helloPage(HttpServletRequest request) {
-//        String name = request.getParameter("name");
-//        String surname = request.getParameter("surname");
-
-        System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message","Hello, " + name + " " + surname);
         return "first/hello";
     }
 
